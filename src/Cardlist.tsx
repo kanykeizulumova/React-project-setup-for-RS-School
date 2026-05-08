37bbfec4efb7fb7a55c9b8b8423990a76b5ebfd2
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-05-08 18:58:20 +0600

    feat: add claslist component

---
+++ src/Cardlist.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card } from './Card.tsx';
+import type { CardProps } from './Card.tsx';
+
+interface CardListProps {
+  items: CardProps[];
+}
+
+class CardList extends React.Component<CardListProps> {
+  render() {
+    const { items } = this.props;
+
+    if (items.length === 0) {
+      return <p>No results found.</p>;
+    }
+
+    return (
+      <div className="card-list">
+        {items.map((char) => (
+          <Card
+            key={char.id}
+            id={char.id}
+            name={char.name}
+            species={char.species}
+            age={char.age}
+            abilities={char.abilities}
+            imageUrl={char.imageUrl}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default CardList;
