f51707beeeeaabae151c917685caa117efc1ab6d
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-05-25 19:11:50 +0600

    feat: fetching data on card details is now done through useQuery

--- src/routes/CharacterDetails.tsx
+++ src/routes/CharacterDetails.tsx
@@ -1,62 +1,52 @@
-import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router';
-import useCharacterStore from '../store/useCharacters';
+import { useQuery } from '@tanstack/react-query';
+import fetchCharacter from '../fetchCharacterDetails';
 
 export default function CharacterDetails() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get('details');
   const query = searchParams.get('query') || '';
   const page = searchParams.get('page') || '1';
-  const character = useCharacterStore((state) => state.selectedCharacter);
-  const isLoading = useCharacterStore((state) => state.isDetailsLoading);
-  const loadData = useCharacterStore((state) => state.fetchCharacterDetails);
-  const error = useCharacterStore((state) => state.detailsError);
-  const clearSelectedCharacter = useCharacterStore(
-    (state) => state.clearSelectedCharacter
-  );
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (id) {
-      loadData(id);
-    }
-
-    return () => {
-      clearSelectedCharacter();
-    };
-  }, [id, loadData, clearSelectedCharacter]);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['characters', id],
+    queryFn: fetchCharacter,
+    enabled: !!id,
+  });
 
   const closeCharacter = () => {
-    clearSelectedCharacter();
     navigate(`/?page=${page}&query=${query}`);
   };
 
   if (isLoading) return <div className="loader">Loading...</div>;
-  if (error) return <div className="error-msg">{error}</div>;
-  if (!character) return <div />;
+  if (error)
+    return <div className="error-msg">{(error as Error)?.message}</div>;
+  if (!data) return <div />;
+
   return (
     <div className="details">
-      <h2>{character.name}</h2>
-      <img src={character.image} alt={character.name} />
+      <h2>{data.name}</h2>
+      <img src={data.image} alt={data.name} />
       <h2>Species:</h2>
-      <p>{character.species}</p>
+      <p>{data.species}</p>
       <h2>Gender:</h2>
-      <p>{character.gender}</p>
+      <p>{data.gender}</p>
       <p>
         Status:{' '}
-        <span className={`status-pill ${character.status.toLowerCase()}`}>
-          {character.status}
+        <span className={`status-pill ${data.status.toLowerCase()}`}>
+          {data.status}
         </span>
       </p>
       <h2>Location:</h2>
-      <p>{character.location.name}</p>
+      <p>{data.location.name}</p>
       <h2>origin: </h2>
-      <p>{character.origin.name}</p>
+      <p>{data.origin.name}</p>
       <h2>Total episode count: </h2>
-      <p>{character.episode.length}</p>
+      <p>{data.episode.length}</p>
       <h3>URL: </h3>
-      <a href={character.url} target="_blank" rel="noreferrer">
-        {character.url}
+      <a href={data.url} target="_blank" rel="noreferrer">
+        {data.url}
       </a>
       <button className="close-btn" type="button" onClick={closeCharacter}>
         X
