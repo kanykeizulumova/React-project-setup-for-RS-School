d5a9024b1d627977668a91a0efd6dc76b8ac6706
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-06-01 21:09:04 +0600

    feat: add modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,40 @@
+import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
+import { formsMap } from '../formsMap';
+
+export default function Modal({ isOpen, onClose, activeForm }) {
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      window.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+    <div className="modal" onClick={onClose}>
+      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button type="button" className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+
+        <div className="form-container">
+          {formsMap[activeForm] || <p>Form not found</p>}
+        </div>
+      </div>
+    </div>,
+    document.getElementById('modal-root')
+  );
+}
---
+++ src/formsMap.tsx
@@ -0,0 +1,11 @@
+import Form from './form';
+
+export const FORMS = {
+  UNCONTROLLED: 'UNCONTROLLED',
+  RHF: 'RHF',
+};
+
+export const formsMap = {
+  [FORMS.UNCONTROLLED]: <Form />,
+  [FORMS.RHF]: <Form />,
+};
