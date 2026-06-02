f828e9d0e480daabe8e20beb36f07e08b94793c3
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-06-02 13:22:00 +0600

    feat: add zustand store to store data

---
+++ src/store/useUserStore.tsx
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export interface UserStore {
+  users: NewUser[];
+  addUser: (newUser?: Partial<Omit<NewUser, 'id'>>) => void;
+  removeUser: (id: number) => void;
+}
+interface NewUser {
+  id: number;
+  fullName?: string;
+  email?: string;
+  age?: number;
+  gender?: string;
+  terms?: boolean;
+}
+
+const useUserStore = create<UserStore>()(
+  persist(
+    (set) => ({
+      users: [],
+
+      addUser: (newUser: NewUser | undefined) =>
+        set((state) => ({
+          users: [...state.users, { ...(newUser ?? {}), id: Date.now() }],
+        })),
+
+      removeUser: (id) =>
+        set((state) => ({
+          users: state.users.filter((user) => user.id !== id),
+        })),
+    }),
+    {
+      name: 'user-storage',
+    }
+  )
+);
+
+export default useUserStore;
