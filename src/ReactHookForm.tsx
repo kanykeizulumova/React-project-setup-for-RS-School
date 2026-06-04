da8859c2ac23b5f1a5423e5ede29a40a7a788c07
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-06-04 15:27:56 +0600

    feat: add upload image in base64 format

--- src/ReactHookForm.tsx
+++ src/ReactHookForm.tsx
@@ -1,4 +1,5 @@
 import { useForm, type SubmitHandler } from 'react-hook-form';
+import type { ChangeEvent } from 'react';
 import useUserStore from './store/useUserStore';
 
 interface IFormInput {
@@ -7,6 +8,7 @@ interface IFormInput {
   gender: string;
   email: string;
   terms: boolean;
+  image: FileList;
 }
 
 export default function ReactHookForm({ onClose }) {
@@ -14,13 +16,45 @@ export default function ReactHookForm({ onClose }) {
 
   const { register, handleSubmit, reset } = useForm<IFormInput>();
 
-  const onSubmit: SubmitHandler<IFormInput> = (data) => {
+  const convertFileToBase64 = (file: File): Promise<string> =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = () => {
+        const result = reader.result as string;
+        const base64String = result.split(',')[1];
+        resolve(base64String);
+      };
+
+      reader.onerror = (error) => reject(error);
+    });
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     try {
-      addUser(data);
+      let base64String: string | undefined;
+
+      if (data.image && data.image.length > 0) {
+        base64String = await convertFileToBase64(data.image[0]);
+      }
+
+      addUser({ ...data, image: base64String });
       reset();
       onClose();
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    if (files && files.length > 0) {
+      try {
+        const base64Result = await convertFileToBase64(files[0]);
+        console.log('Base64 String:', base64Result);
+      } catch (error) {
+        console.error('Conversion failed:', error);
+      }
     }
   };
 
@@ -29,6 +63,7 @@ export default function ReactHookForm({ onClose }) {
   const emailReg = register('email');
   const genderReg = register('gender');
   const termsReg = register('terms');
+  const imageReg = register('image');
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="modal-form">
@@ -93,6 +128,22 @@ export default function ReactHookForm({ onClose }) {
           required
         />{' '}
         Female
+      </label>
+
+      <label htmlFor="userFiles">
+        Upload image:
+        <input
+          id="userFiles"
+          type="file"
+          name={imageReg.name}
+          onChange={(e) => {
+            imageReg.onChange(e);
+            handleFileChange(e);
+          }}
+          onBlur={imageReg.onBlur}
+          ref={(element) => imageReg.ref(element)}
+          placeholder="Upload your image"
+        />
       </label>
 
       <label
