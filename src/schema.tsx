afda215228b7b759f29301b4c23ffd29607f3f5c
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-06-05 17:01:26 +0600

    feat: add more validation test to schema

--- src/schema.tsx
+++ src/schema.tsx
@@ -1,45 +1,72 @@
 import * as yup from 'yup';
 
-const schema = yup.object({
-  fullName: yup
-    .string()
-    .required('Name is required')
-    .matches(/^[A-Z]/, 'The first letter must be uppercase'),
-  age: yup
-    .number()
-    .positive('Возраст должен быть положительным числом')
-    .integer()
-    .required('Возраст обязателен'),
+const MAX_FILE_SIZE = 2 * 1024 * 1024;
+const SUPPORTED_FORMATS = ['image/jpeg', 'image/jpg', 'image/png'];
 
-  email: yup
-    .string()
-    .email('Некорректный формат email')
-    .required('Email обязателен'),
+const schema = yup
+  .object({
+    fullName: yup
+      .string()
+      .defined()
+      .required('Name is required')
+      .matches(/^[A-Z]/, 'The first letter must be uppercase'),
+    age: yup
+      .number()
+      .positive('Age must be a positive number')
+      .integer()
+      .defined()
+      .required('Age is required'),
 
-  gender: yup
-    .string()
-    .oneOf(['male', 'female'], 'Please select a valid gender option')
-    .required('Gender selection is required'),
+    email: yup
+      .string()
+      .email('Incorrect email format')
+      .defined()
+      .required('Email is required'),
 
-  terms: yup.boolean(),
+    gender: yup
+      .string<'male' | 'female'>()
+      .oneOf(['male', 'female'], 'Please select a valid gender option')
+      .defined()
+      .required('Gender selection is required'),
 
-  image: yup.mixed(),
+    terms: yup.boolean().defined().required(),
 
-  password: yup
-    .string()
-    .required('Password is required')
-    .min(8, 'Must be at least 8 characters')
-    .matches(/[a-z]/, 'Must contain a lowercase letter')
-    .matches(/[A-Z]/, 'Must contain an uppercase letter')
-    .matches(/\d/, 'Must contain a number')
-    .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Must contain a special character'),
+    image: yup
+      .mixed<File>()
 
-  confirmPassword: yup
-    .string()
-    .oneOf([yup.ref('password')], 'Пароли должны совпадать')
-    .required('Повторите пароль'),
+      .test(
+        'fileSize',
+        'File size is too large (max 2MB)',
+        (value) => value && value.size <= MAX_FILE_SIZE
+      )
 
-  country: yup.string(),
-});
+      .test(
+        'fileType',
+        'Unsupported file format',
+        (value) => value && SUPPORTED_FORMATS.includes(value.type)
+      )
+      .defined()
+      .required('An image is required'),
+
+    password: yup
+      .string()
+      .min(8, 'Must be at least 8 characters')
+      .matches(/[a-z]/, 'Must contain a lowercase letter')
+      .matches(/[A-Z]/, 'Must contain an uppercase letter')
+      .matches(/\d/, 'Must contain a number')
+      .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Must contain a special character')
+      .defined()
+      .required('Password is required'),
+
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password')], 'Password must match')
+      .defined()
+      .nonNullable()
+      .required('Repeat password'),
+
+    country: yup.string().defined().required(),
+  })
+  .required();
 
 export default schema;
