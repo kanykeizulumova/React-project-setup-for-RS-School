d0258e6c3247c72611aef5abae65458f0e7b5d33
kanykeizulumova <zulumovakanykei54@gmail.com>
2026-06-16 16:01:02 +0600

    feat: add next config and main settings

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    distDir: './dist',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+}
+
+export default nextConfig
+
+
